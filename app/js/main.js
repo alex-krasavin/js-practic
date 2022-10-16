4d1f243153032be013aca586865637d252b0522b
Aleksandr <krasa19920906@gmail.com>
2022-10-16 18:51:31 +0300

    js movie app

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
-// Обьявление переменной let, имя переменной,присваивание = значение переменной;
-let number =5;
-const leftBorderWidth = 1;
+'use strict';
+const numberOfFilms = +prompt ("Сколько фильмов вы уже посмотрели?", "");
+const personalMovieDataBase = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const lastSeeMovie = prompt ("Один из последних просмотренных фильмов?", "");
+const filmScore = prompt ("На сколько оцените его?", "");
+
+personalMovieDataBase.movies [lastSeeMovie] = filmScore;
+
+console.log (personalMovieDataBase);
